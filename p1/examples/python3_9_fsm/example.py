34fcc69601c0dfeb830fe39b67297c1649fabdfb
nefanov <efanov@phystech.edu>
2022-01-31 14:15:10 +0300

    Create example.py

---
+++ p1/examples/python3_9_fsm/example.py
@@ -0,0 +1,34 @@
+# perform "pip install transitions", then:
+# Original: https://habr.com/ru/post/521946/
+from transitions import Machine
+
+# На этот объект будем вешать состояния
+class Matter(object):
+    pass
+
+lump = Matter()
+
+# Полный список состояний
+states=['solid', 'liquid', 'gas', 'plasma']
+
+# Добавляем таблицу переходов — из какое в какое состояние мы можем попасть
+transitions = [
+    { 'trigger': 'melt', 'source': 'solid', 'dest': 'liquid' },
+    { 'trigger': 'evaporate', 'source': 'liquid', 'dest': 'gas' },
+    { 'trigger': 'sublimate', 'source': 'solid', 'dest': 'gas' },
+    { 'trigger': 'ionize', 'source': 'gas', 'dest': 'plasma' }
+]
+
+# Инициализация машины
+machine = Machine(lump, states=states, transitions=transitions, initial='liquid')
+
+# Проверяем начальное состояние
+lump.state
+# >>> 'liquid'
+
+# И пробуем изменить состояние триггерами перехода
+lump.evaporate()
+lump.state
+# >>> 'gas'
+lump.trigger('ionize')
+lump.state
