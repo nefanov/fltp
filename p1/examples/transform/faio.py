fa3743520f18b3ed6c262c55b4fc4d4baf0b2f7c
nefanov <efanov@phystech.edu>
2022-01-31 20:22:15 +0300

    Update faio.py

--- p1/examples/transform/faio.py
+++ p1/examples/transform/faio.py
@@ -6,12 +6,21 @@ nfa = {}
 nfa_states = []
 dfa_states = []
 
+def check_input_correctness():
+    if sys.argv<=2:
+        print("Input is incorrect")
+        sys.exit(1)
+    else:
+        return
+
 def load_nfa():
+    check_input_correctness()
     global nfa
     with open(sys.argv[1], 'r') as inpjson:
         nfa = json.loads(inpjson.read())
 
 def out_dfa():
+    check_input_correctness()
     global dfa
     with open(sys.argv[2], 'w') as outjson:
         outjson.write(json.dumps(dfa, indent = 4))
