a78a99693e7ef062d7f159f726f83a9f96bbc6fe
nefanov <efanov@phystech.edu>
2022-02-01 20:45:17 +0300

    Update nfa2dfa.py

--- p1/examples/transform/nfa2dfa.py
+++ p1/examples/transform/nfa2dfa.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     for state in nfa['states']:
         nfa_states.append(state) # Q_0 DFA == Q NFA
 
-    dfa_states = get_power_set(nfa_states)[1:] # set of sets of the states\emptyset
+    dfa_states = get_power_set(nfa_states)[1:] # set of sets of the states
 
     dfa['states'] = []
     for states in dfa_states:
@@ -37,8 +37,7 @@ if __name__ == "__main__":
             q_states = []
             for i in states:
                 q_states.append(i)
-            dfa['transition_function'].append([q_states, letter, q_to])
-
+            dfa['transition_function'].append([sorted(q_states), letter, sorted(q_to)])
     dfa['start_states'] = []
     for state in nfa['start_states']:
         dfa['start_states'].append([state])
@@ -48,4 +47,18 @@ if __name__ == "__main__":
             if state in nfa['final_states'] and states not in dfa['final_states']:
                 dfa['final_states'].append(states)
     
+    # finalize labels as strings:
+    for i, item in enumerate(dfa['states']):
+        dfa['states'][i] = "_".join(item)
+
+    for i, item in enumerate(dfa['start_states']):
+        dfa['start_states'][i] = "_".join(item)
+
+    for i, item in enumerate(dfa['final_states']):
+        dfa['final_states'][i] = "_".join(item)
+    print(dfa['transition_function'])
+    for i, item in enumerate(dfa['transition_function']):
+        dfa['transition_function'][i][0] = "_".join(item[0])
+        dfa['transition_function'][i][2] = "_".join(item[2])
     faio.out_dfa(dfa)
+    
