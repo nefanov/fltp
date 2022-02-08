acaadbb5e59b7a8672d5c22e9656dc7f97d64073
nefanov <efanov@phystech.edu>
2022-02-08 13:47:21 +0300

    Update faio.py

--- p1/examples/transform/faio.py
+++ p1/examples/transform/faio.py
@@ -31,9 +31,24 @@ def load_fa():
 def out_dfa(fa):
     out_fa(fa)
 
-def out_fa(fa):
+def out_fa(fa, need_finalize_for_plot=True):
     check_input_correctness()
     with open(sys.argv[2], 'w') as outjson:
+        if need_finalize_for_plot:
+        # finalize labels as strings:
+            for i, item in enumerate(fa['states']):
+                fa['states'][i] = "_".join(item)
+
+            for i, item in enumerate(fa['start_states']):
+                fa['start_states'][i] = "_".join(item)
+
+            for i, item in enumerate(fa['final_states']):
+                fa['final_states'][i] = "_".join(item)
+
+            for i, item in enumerate(fa['transition_function']):
+                fa['transition_function'][i][0] = "_".join(item[0])
+                fa['transition_function'][i][2] = "_".join(item[2])
+        
         outjson.write(json.dumps(fa, indent = 4))
 
 if __name__ == '__main__':
