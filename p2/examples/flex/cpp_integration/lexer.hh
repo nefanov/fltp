41ffa0510c89232ab145adb21b5134b405f1cccb
nefanov <efanov@phystech.edu>
2022-02-14 21:23:35 +0300

    Update lexer.hh

--- p2/examples/flex/cpp_integration/lexer.hh
+++ p2/examples/flex/cpp_integration/lexer.hh
@@ -3,12 +3,12 @@
 #if ! defined(yyFlexLexerOnce)
 #include <FlexLexer.h>
 #endif
-  
+
 #undef YY_DECL
 #define YY_DECL int Lexer::ScanToken()
-  
+
 class Lexer: public yyFlexLexer {
-public:
-  virtual ~Lexer() {}
-  virtual int ScanToken();
+ public:
+	virtual ~Lexer() {}
+	virtual int ScanToken();
 };
