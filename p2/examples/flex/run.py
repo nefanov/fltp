3f1fd086cb0def91d67e4a7db5db106e72fd2512
nefanov <efanov@phystech.edu>
2022-02-14 12:55:05 +0300

    Create run.py

---
+++ p2/examples/flex/run.py
@@ -0,0 +1,16 @@
+import subprocess
+import sys
+
+# flex получает на вход sys.argv[1] и генерирует sys.argv[2].
+
+inp = "input.l"
+outp = "output.c"
+
+if "-h" in sys.argv:
+  print("usage: python3 run.py inp_file outp_file")
+  sys.exit(0)
+
+if (sys.argv) > 2:
+  inp = sys.argv[1]
+  outp = sys.argv[1]
+subprocess.check_call(['flex', '-o', outp, inp])
